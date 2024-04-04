ba99b2c58caa22ac1c14a5e458a837ae37f06afa
Vladislav Plotnikov <vladik1209@gmail.com>
2024-04-04 21:02:51 +0300

    disable bottom

--- run_docker_vision.sh
+++ run_docker_vision.sh
@@ -1,7 +1,6 @@
 docker run --rm -it --gpus all \
     --ipc=host \
     --device="/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._H264_USB_Camera_SN0001-video-index0" \
-    --device="/dev/v4l/by-id/usb-Microsoft_Microsoft®_LifeCam_HD-3000-video-index0" \
     --net=host \
     hydronautics/welt_auv:vision \
     bash
