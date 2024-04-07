ab6deeefe7291a4a59ef1ba3646169a898778969
Vladislav Plotnikov <vladik1209@gmail.com>
2024-04-08 00:21:12 +0300

    change jetson board

--- run_docker_vision.sh
+++ run_docker_vision.sh
@@ -1,8 +1,7 @@
 docker run --rm -it --gpus all \
     -v $HOME/records:/welt_auv/records \
     --ipc=host \
-    --device="/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._H264_USB_Camera_SN0001-video-index0" \
-    --device="/dev/v4l/by-id/usb-Microsoft_Microsoft®_LifeCam_HD-3000-video-index0" \
+    --device="/dev/video0" \
     --net=host \
     hydronautics/welt_auv:vision \
     bash
