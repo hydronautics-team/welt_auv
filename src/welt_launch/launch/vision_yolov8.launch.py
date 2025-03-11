f17c698e93f9e8acfda94cc72d37b5db865a9d33
Пак Леонард Алексеевич <lapak@sberautotech.ru>
2025-03-11 11:35:42 +0300

    left_raw -> left

--- src/welt_launch/launch/vision_yolov8.launch.py
+++ src/welt_launch/launch/vision_yolov8.launch.py
@@ -1,16 +1,12 @@
 from pathlib import Path
 
 from ament_index_python import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.actions import GroupAction
+from launch.actions import (DeclareLaunchArgument, GroupAction,
+                            IncludeLaunchDescription)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import TextSubstitution
-from launch_ros.actions import Node
-from launch_ros.actions import PushRosNamespace
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch_ros.actions import Node, PushRosNamespace
 
 
 def generate_launch_description():
@@ -20,7 +16,7 @@ def generate_launch_description():
     )
     # object detection
     image_topic_list_arg = DeclareLaunchArgument(
-        "image_topic_list", default_value='[/zed/zed_node/left_raw/image_raw_color]'
+        "image_topic_list", default_value='[/zed/zed_node/left/image_raw_color]'
     )
     weights_pkg_name_arg = DeclareLaunchArgument(
         "weights_pkg_name", default_value='sauvc_object_detection'
