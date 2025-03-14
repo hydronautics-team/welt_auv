452dc8b951ac16d1437f7fd447430de87914bff0
Vladislav Plotnikov <vladik1209@gmail.com>
2025-03-14 11:41:46 +0300

    build-od: fix entrypoint

--- docker/od/entrypoint.sh
+++ docker/od/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "[DOCKER-INFO] Запускаем сборку..."
+# echo "[DOCKER-INFO] Запускаем сборку..."
 # Выполняем сборку, при ошибке выходим
 
 source "/opt/ros/humble/install/setup.bash"
 source "/additional_packages/install/setup.bash"
 source /welt_auv/install/setup.bash
 
-if ! ./bv; then
-  echo "[DOCKER-ERROR] Сборка завершилась с ошибкой. Выходим..."
-  exit 1
-fi
-echo "[DOCKER-INFO] Сборка завершена успешно. Выполняем source install/setup.bash..."
-source /welt_auv/install/setup.bash
+# if ! ./bv; then
+#   echo "[DOCKER-ERROR] Сборка завершилась с ошибкой. Выходим..."
+#   exit 1
+# fi
+# echo "[DOCKER-INFO] Сборка завершена успешно. Выполняем source install/setup.bash..."
+# source /welt_auv/install/setup.bash
 
 exec "$@"
